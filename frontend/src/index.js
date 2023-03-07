21c328e75a1221b211cbdefbb6bcd310355b2646
Bruno Alves <bruno6821@gmail.com>
2023-03-07 23:04:02 +0100

    Add Bootstrapp

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import '../src/assets/theme/bootstrap.min.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
